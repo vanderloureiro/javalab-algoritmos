0d7d400a328291d23628ef45d8aad25e37fcb441
Vanderlei Loureiro <vanderloureiroleite@gmail.com>
2023-08-25 21:54:13 -0300

    fatorial

---
+++ Fatorial.java
@@ -0,0 +1,11 @@
+public class Fatorial {
+    
+    public static void main(String args[]) {
+        int valor = 1;
+        int fatorial = 10;
+        for (int i = fatorial; fatorial > 0; fatorial--) {
+            valor = fatorial * valor;
+        }
+        System.out.println(valor);
+    }
+}
