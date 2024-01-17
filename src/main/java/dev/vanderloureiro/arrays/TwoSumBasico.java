ce0768b256e1e29a4f90ff7fa07fc2f4c28a8bf1
vanderlei <vanderloureiroleite@gmail.com>
2024-01-16 22:24:46 -0300

    correcao em twoSumBasico

--- src/main/java/dev/vanderloureiro/arrays/TwoSumBasico.java
+++ src/main/java/dev/vanderloureiro/arrays/TwoSumBasico.java
@@ -27,7 +27,7 @@ public class TwoSumBasico {
     public static int[] executa(int[] nums, int alvo) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = 0; j < nums.length; j++) {
-                if (nums[i] == nums[j]) {
+                if (i == j) {
                     continue;
                 }
                 if (nums[i] + nums[j] == alvo) {
