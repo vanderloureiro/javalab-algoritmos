1946dd6dcdfd0a2ccab78c6b6a58249edd5eb6c3
vanderlei <vanderloureiroleite@gmail.com>
2024-01-22 22:20:28 -0300

    segunda forma de inversao de String

--- src/main/java/dev/vanderloureiro/outros/InverterString.java
+++ src/main/java/dev/vanderloureiro/outros/InverterString.java
@@ -9,12 +9,23 @@ public class InverterString {
     public void inverterString() {
         String inversao = executa("hello");
         Assertions.assertEquals("olleh", inversao);
+
+        String inversao2 = executaNovaForma("World");
+        Assertions.assertEquals("dlroW", inversao2);
     }
 
     public String executa(String texto) {
         String inversao = "";
         for (int i = texto.length() - 1; i >= 0; i--) {
             inversao += texto.charAt(i);
+        }
+        return inversao;
+    }
+
+    public String executaNovaForma(String texto) {
+        String inversao = "";
+        for (int i = 0; i < texto.length(); i++) {
+            inversao = texto.charAt(i) + inversao;
         }
         return inversao;
     }
