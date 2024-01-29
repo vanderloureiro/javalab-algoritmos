17a72d7abfbea170f998dd5f79a89747349c8b16
vanderlei <vanderloureiroleite@gmail.com>
2024-01-29 18:34:54 -0300

    refatoracao de inverter String com loop

--- src/main/java/dev/vanderloureiro/outros/InverterString.java
+++ src/main/java/dev/vanderloureiro/outros/InverterString.java
@@ -7,26 +7,16 @@ public class InverterString {
 
     @Test
     public void inverterString() {
-        String inversao = executa("hello");
-        Assertions.assertEquals("olleh", inversao);
-
-        String inversao2 = executaNovaForma("World");
+        String inversao2 = executa("World");
         Assertions.assertEquals("dlroW", inversao2);
     }
 
-    public String executa(String texto) {
-        String inversao = "";
-        for (int i = texto.length() - 1; i >= 0; i--) {
-            inversao += texto.charAt(i);
-        }
-        return inversao;
-    }
 
-    public String executaNovaForma(String texto) {
-        String inversao = "";
+    public String executa(String texto) {
+        StringBuilder inversao = new StringBuilder();
         for (int i = 0; i < texto.length(); i++) {
-            inversao = texto.charAt(i) + inversao;
+            inversao.insert(0, texto.charAt(i));
         }
-        return inversao;
+        return inversao.toString();
     }
 }
