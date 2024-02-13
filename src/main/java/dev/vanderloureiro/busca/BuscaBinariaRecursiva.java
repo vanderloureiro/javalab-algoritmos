fc618cdf6d563622b58c43e229c46beb4501fb0b
vanderlei <vanderloureiroleite@gmail.com>
2024-02-13 20:50:52 -0300

    refatoracao de busca binária recursiva

--- src/main/java/dev/vanderloureiro/busca/BuscaBinariaRecursiva.java
+++ src/main/java/dev/vanderloureiro/busca/BuscaBinariaRecursiva.java
@@ -3,33 +3,35 @@ package dev.vanderloureiro.busca;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-
 public class BuscaBinariaRecursiva {
 
     @Test
     public void buscaBinaria() {
         int[] numeros = {2, 7, 11, 15, 35, 48, 52, 63, 64, 65, 70};
-        int alvo = 65;
+        int alvo = 35;
         int resposta = executa(numeros, alvo);
-        Assertions.assertEquals(alvo, resposta);
+        Assertions.assertEquals(4, resposta);
     }
 
     private int executa(int[] lista, int elemento) {
-        int meio = lista.length / 2;
+        return this.executa(lista, elemento, 0, lista.length - 1);
+    }
+
+    private int executa(int[] lista, int elemento, int inicio, int fim) {
+
+        int meio = ((fim - inicio) / 2) + inicio;
+
         if (lista[meio] == elemento) {
-            return lista[meio];
+            return meio;
         }
-        if (lista.length == 1) {
+        if (fim - inicio == 1) {
             return -1;
         }
         if (elemento < lista[meio]) {
-            int[] novaLista = Arrays.copyOfRange(lista, 0, meio);
-            return executa(novaLista, elemento);
+            return executa(lista, elemento, inicio, meio);
         }
         if (elemento > lista[meio]) {
-            int[] novaLista = Arrays.copyOfRange(lista, meio, lista.length);
-            return executa(novaLista, elemento);
+            return executa(lista, elemento, meio, fim);
         }
         return -1;
     }
