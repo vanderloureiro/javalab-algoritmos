86f1d8ba739c66ac394ebffc3192638c426ed8bf
vanderlei <vanderloureiroleite@gmail.com>
2024-02-29 20:01:19 -0300

    algoritmo de lacuna binária

--- src/main/java/dev/vanderloureiro/strings/LacunaBinaria.java
+++ src/main/java/dev/vanderloureiro/strings/LacunaBinaria.java
@@ -1,5 +1,7 @@
 package dev.vanderloureiro.strings;
 
+import org.junit.jupiter.api.Assertions;
+
 public class LacunaBinaria {
 
     /*
@@ -8,13 +10,26 @@ public class LacunaBinaria {
     public static void main(String args[]) {
 
         // 529 = 1000010001
-        int representacao[] = {1,0,0,0,0,1,0,0,0,1};
-        String represent = Integer.toString(529, 2);
-        System.out.println(represent);
+        // int representacao[] = {1,0,0,0,0,1,0,0,0,1};
+        int resposta = lacunaBinaria(529);
+        Assertions.assertEquals(4, resposta);
+    }
+
+    private static int lacunaBinaria(int n) {
+        String representacao = Integer.toBinaryString(n);
         int maiorLacuna = 0;
-        for (int i : representacao) {
-            System.out.println(i);
+        int lacunaCorrente = 0;
+        for (int i = 0; i < representacao.length(); i++) {
+            if (representacao.charAt(i) == '0') {
+                lacunaCorrente++;
+                if (lacunaCorrente > maiorLacuna) {
+                    maiorLacuna = lacunaCorrente;
+                }
+            } else {
+                lacunaCorrente = 0;
+            }
         }
+        return maiorLacuna;
     }
 
 }
